9f42f9d73f6e6361a4d0c32041ff323133656e59
Francisco Assis Silva <f.assisferreira@icloud.com>
2025-11-16 17:09:15 -0300

    adiciona o arquivo 'lint:check' e 'lint:fix'

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto de Desenvolvimento de um Portal de Notícias
+
+## O lognotícias é um projeto que está sendo desenvolvido para conclusão do curso de extensão de desenvolvimento web com JavaScript
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Estamos constuindo uma página de notícias do mundo logístico de Goiás!!! Aguardem!!</h1>;
+  return (
+    <h1>
+      Estamos constuindo uma página de notícias do mundo logístico de Goiás!!!
+      Aguardem!!
+    </h1>
+  );
 }
 
 export default Home;
